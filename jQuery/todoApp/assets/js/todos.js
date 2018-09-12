4125d7b2e1243459b7e98ab1237c9f4acee79523
Ermyas Fekadu <ermyas.fekadu@gmail.com>
2018-09-12 12:10:30 +0530

    todo App complete

--- jQuery/todoApp/assets/js/todos.js
+++ jQuery/todoApp/assets/js/todos.js
@@ -16,6 +16,10 @@ $("input[type='text']").keypress(function(event){
 	if(event.which === 13){
 		var inputText = $(this).val();
 		$(this).val("");
-		$('ul').append(`<li><span>X </span>${inputText}</li>`);
+		$('ul').append(`<li><span><i class="fas fa-trash-alt"></i> </span>${inputText}</li>`);
 	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").toggleClass("content");
 });
