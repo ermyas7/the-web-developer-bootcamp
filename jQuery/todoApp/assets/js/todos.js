4058765bbfc8a7421354e557d181f04c05f6309b
Ermyas Fekadu <ermyas.fekadu@gmail.com>
2018-09-12 10:07:40 +0530

    todoApp logic complete

--- jQuery/todoApp/assets/js/todos.js
+++ jQuery/todoApp/assets/js/todos.js
@@ -1,6 +1,21 @@
-//check the clicked li
+//check the todo list
 
-$('li').click(function(){
+$('ul').on("click", "li", function(){
 	$(this).toggleClass("complete");
 });
 
+//remove the todo list
+$('ul').on("click", "span",function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		var inputText = $(this).val();
+		$(this).val("");
+		$('ul').append(`<li><span>X </span>${inputText}</li>`);
+	}
+});
