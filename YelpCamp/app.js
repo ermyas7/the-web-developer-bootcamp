d4df070a16fdb358ea452820b27ccbeda8387332
Ermyas <ermyas.fekadu@gmail.com>
2018-10-10 16:52:54 +0000

    styling show page complete

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -10,6 +10,8 @@ express = require("express");
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+//serve public directory
+app.use(express.static(__dirname+"/public"));
 
 //database config
 mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
